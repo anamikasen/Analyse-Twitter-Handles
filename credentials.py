66b02fe66d0e21b01914f6c77bd09f3237f57ee8
anamikasen <anamika13sen@gmail.com>
2018-06-03 20:52:21 -0400

    credentials file with API keys

---
+++ credentials.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Twitter App access keys for @anamiikaaa
+# The API keys are not to be disclosed.
+# Consume:
+CONSUMER_KEY    = ''
+CONSUMER_SECRET = ''
+
+# Access:
+ACCESS_TOKEN  = ''
+ACCESS_SECRET = ''
+
